cd4c54519ac3c0e0791346ae7e2f5a2dc2e5243b
beninovak <beni.novak13@gmail.com>
2023-06-07 20:17:28 +0200

    Add project files.

---
+++ MyMath.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberTypes
+{
+    internal class MyMath
+    {
+        /// <summary>
+        ///     Set <i>proper</i> to <b>true</b> if you wish to exclude the number itself from the returned array.<br/>
+        ///     Set <i>exlude_1</i> to <b>true</b> if you wish to exclude 1 from the returned array.<br/>
+        ///     Set <i>sort</i> to <b>true</b> if you wish to sort the array ascendingly, or to <b>false</b> if you wish to sort it descendingly.<br/>
+        ///     If left unspecified or set to <b>null</b>, the array will be unchanged.
+        /// </summary>
+        /// <returns>Integer array of divisors of the passed number.</returns>
+        public List<int> GetDivisors(int num, bool proper = false, bool exclude_1 = false, bool? sort = null)
+        {
+            List<int> divisors = new List<int>();
+
+            if (!exclude_1)
+            {
+                divisors.Add(1);
+            }
+
+            for (int i = 2; i <= Math.Floor(Math.Sqrt(num)); i++)
+            {
+                if (num % i == 0)
+                {
+                    divisors.Add(i);
+
+                    if (i != (num / i))
+                    {
+                        divisors.Add(num / i);
+                    }
+                }
+            }
+
+            if (!proper)
+            {
+                divisors.Add(num);
+            }
+
+            if (sort != null)
+            {
+                if (sort == true)
+                {
+                    divisors.Sort();
+                } else {
+                    divisors.Sort();
+                    divisors.Reverse();
+                }
+            }
+
+            return divisors;
+        }
+
+        /// <summary>
+        ///     Set <i>proper</i> to <b>true</b> if you wish to exclude the number itself from the sum of its divisors.<br/>
+        ///     Set <i>exlude_1</i> to <b>true</b> if you wish to exclude it from the sum of divisors.<br/>
+        /// </summary>
+        /// <returns>Sum of divisors of passed number.</returns>
+        public int GetDivisorSum(int num, bool proper = false, bool exclude_1 = false)
+        {
+            int sum = 0;
+            List<int> divisors = GetDivisors(num, proper, exclude_1);
+            divisors.ForEach(i => sum += i);
+
+            return sum;
+        }
+
+        /// <returns>Sum of array elements.</returns>
+        public int GetArraySum(List<int> divisors)
+        {
+            int sum = 0;
+            divisors.ForEach(i => sum += i);
+
+            return sum;
+        }
+
+        /// <returns>Number of proper divisors of passed number, excluding 1.</returns>
+        public int GetDivisorCount (int num)
+        {
+            return GetDivisors(num, true, true).Count;
+        }
+        
+        /// <returns>Returns boolean indicating whether the passed number is a prime number.</returns>
+        public bool IsPrime(int num)
+        {
+            if (num == 1) return false;
+            if (num == 2) return true;
+            if (num % 2 == 0) return false;
+
+            if (GetDivisorCount(num) == 0) return true;
+            else return false;
+        }
+
+        /// <returns>
+        ///     Returns boolean indicating whether the passed numbers are coprime.<br/>
+        ///     <b><i><a href="https://en.wikipedia.org/wiki/Coprime_integers"></a></i></b>
+        /// </returns>
+        public bool AreCoprime(int a, int b) {
+            List<int> divsA = GetDivisors(a, true, true);
+            List<int> divsB = GetDivisors(b, true, true);
+                     
+            return !divsA.Intersect(divsB).Any();
+        }
+    }
+}
---
+++ NumberTypes.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Transactions;
+
+namespace NumberTypes
+{ 
+    internal class NumberTypes
+    {
+        public static MyMath MM = new MyMath();
+
+        static void Main()
+        {
+            var NT = new NumberTypes();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // Perfect numbers
+            Console.WriteLine($"Perfect numbers up to 10000: {NT.PerfectNumbers(10000)}");
+            Console.WriteLine($"Finding perfect numbers took {(float)stopwatch.ElapsedMilliseconds/1000} seconds\n");
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            // Abundant
+            //Console.WriteLine($"Abundant numbers up to 100: {NT.AbundantNumbers(100)}");
+            //Console.WriteLine($"Finding abundant numbers took {(float)stopwatch.ElapsedMilliseconds / 1000} seconds\n");
+            //stopwatch.Reset();
+            //stopwatch.Start();
+
+            // Amicable numbers
+            Console.WriteLine($"Amicable numbers up to 100000: {NT.AmicableNumbers(100000)}");
+            Console.WriteLine($"Amicable numbers took {(float)stopwatch.ElapsedMilliseconds / 1000} seconds\n");
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            // Powerful numbers
+            Console.WriteLine($"Powerful numbers from 100 to 1000: {NT.PowerfulNumbers(1000)}");
+            Console.WriteLine($"Powerful numbers took {(float)stopwatch.ElapsedMilliseconds / 1000} seconds\n");
+
+
+            Console.ReadKey();
+        }
+
+        // A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself.
+        // https://en.wikipedia.org/wiki/Perfect_number
+        public string PerfectNumbers(int upTo) {
+
+            List<int> numbers = new List<int>();
+
+            for (int i = 2; i <= upTo; i++)
+            {
+                if (MM.GetDivisorSum(i, true, false) == i)
+                {
+                    numbers.Add(i);
+                }
+            }
+
+            return string.Join(", ", numbers.ToArray());
+        }
+
+        // An abundant number or excessive number is a number for which the sum of its proper divisors is greater than the number.
+        // https://en.wikipedia.org/wiki/Abundant_number
+        public string AbundantNumbers(int upTo)
+        {
+            List<int> numbers = new List<int>();
+
+            for (int i = 2; i <= upTo; i ++)
+            {
+                if (MM.GetDivisorSum(i, true, false) > i)
+                {
+                    numbers.Add(i);
+                }
+            }
+
+            return string.Join(", ", numbers.ToArray());
+        }
+
+        // Amicable numbers are two different natural numbers related in such a way that the sum of the proper divisors of each is equal to the other number.
+        // https://en.wikipedia.org/wiki/Amicable_numbers
+        public string AmicableNumbers(int upTo)
+        {
+            int divSum = 0;
+            List<int> numbers = new List<int>();
+
+            for (int i = 2; i <= upTo; i++)
+            {
+                divSum = MM.GetDivisorSum(i, true, false);
+
+                // First condition filters out perfect numbers, the second eliminates duplication
+                if (divSum == i || numbers.Contains(divSum)) continue;
+                
+                if (MM.GetDivisorSum(divSum, true, false) == i)
+                {
+                    numbers.Add(i);
+                    numbers.Add(divSum);
+                }
+            }
+
+            return string.Join(", ", numbers.ToArray());
+        }
+
+        // A powerful number is a positive integer m such that for every prime number p dividing m, p2 also divides m.
+        // https://en.wikipedia.org/wiki/Powerful_number
+        public string PowerfulNumbers(int upTo)
+        {
+            bool allPrimeSquaresDivide;
+            List<int> divs = new List<int>();
+            List<int> primeDivs = new List<int>();
+            List<int> numbers = new List<int>();
+
+            for (int i = 100; i <= upTo; i++)
+            {
+                if (MM.IsPrime(i)) continue;
+
+                allPrimeSquaresDivide = true;
+                divs = MM.GetDivisors(i, true, true);
+                primeDivs = divs.Where(x => MM.IsPrime(x)).ToList();
+
+                foreach (int num in primeDivs)
+                {
+                    if (i % (num * num) == 0) continue;
+
+                    // If the square of ANY of the prime divisors does not divide 'i',
+                    // the conditions for a powerful number are not satisfied.
+                    else
+                    {
+                        allPrimeSquaresDivide = false;
+                        break;
+                    }
+                }
+
+                if (allPrimeSquaresDivide) {
+                    numbers.Add(i);
+                }
+            }
+
+            return string.Join(", ", numbers.ToArray());
+        }
+    }
+}
